ce4a834c31e4700a9c9b08fc505c8a384faecea2
Przemyslaw Waberski <przemekwaberski@gmai.l.com>
2019-04-09 22:06:58 +0200

    Dodano pobieranie listy produktów, nie udało się znaleźć przycisku następnej strony

--- src/test/java/XKomFactory.java
+++ src/test/java/XKomFactory.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,14 +16,19 @@ public class XKomFactory {
     @FindBy(xpath = "//*[@id=\"searchBar\"]/div[1]/div[3]/button")
     WebElement searchBtn;
 
-    @FindBy(xpath = "//*[@id=\"productListUtilitiesBar\"]/div[4]/div/span/span")
+    @FindBy(xpath = "//*[@id=\"pageWrapper\"]/div[3]/div[2]/div[2]/div[5]/div/div/ul/li[5]/a")
     WebElement searchAmount;
 
-    @FindBy(xpath = "//*[@id=\"productListUtilitiesBar\"]/div[4]/div/a[2]")
-    WebElement NextSiteBtn;
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[3]/div[2]/div[2]/div[3]/div[4]/div[1]/a[2]")//nie działa
+    WebElement nextSiteBtn;
 
-    @FindBy(className = "product-item product-impression")
-    List<WebElement> Products;
+    @FindBy(className = "name")
+    WebElement product;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[3]/div[1]/div[7]")
+    WebElement specification;
+
+
 
 
 
@@ -31,8 +37,34 @@ public class XKomFactory {
         PageFactory.initElements(driver,this);
     }
 
-public void productSearch(String product){
+    public void productSearch(String product){
         searchField.sendKeys(product);
         searchBtn.click();
+    }
+
+    public void printProducts(){
+
+        List<WebElement> products = driver.findElements(By.className("name"));
+        for (int i = 0; i < products.size(); i++){
+            System.out.println(products.get(i).getAttribute("title"));
+        }
+    }
+
+    public void productDetails(){
+        product.click();
+        System.out.println(specification.getText());
+    }
+
+    public void checkForMoreSites(){
+       // int val = Integer.parseInt(searchAmount.getText());
+        //System.out.println(nextSiteBtn.isEnabled());
+        nextSiteBtn.click();
+      /*  if(val > 1){
+            return true;
+        }*/
+    }
+
+    public void nextSite(){
+        nextSiteBtn.click();
     }
 }
---
+++ src/test/java/XKomTest.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class XKomTest {
+
+    WebDriver driver;
+    XKomFactory xFactory;
+    String product;
+
+
+    @BeforeTest
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-java-3.141.59\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(100, TimeUnit.MILLISECONDS);
+        driver.get("https://www.x-kom.pl/");
+        product = JOptionPane.showInputDialog(null,"Podaj nazwę produktu");
+    }
+
+    @Test
+    public void searchForSth() throws IOException {
+        xFactory = new XKomFactory(driver);
+        xFactory.productSearch(product);
+        xFactory.printProducts();
+    }
+
+    @Test
+    public void nextSite(){
+       xFactory.checkForMoreSites();
+    }
+/*
+    @AfterTest
+    public void cleanUp(){
+        driver.close();
+    }*/
+}
